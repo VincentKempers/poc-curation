0d88fc7c7e5654178eb4eab0d616e028fe2a2db1
Vincent Kempers <me@vncn.tk>
2018-12-11 15:11:46 +0100

    add the routers and deleted alfred.vue

--- application/src/router.js
+++ application/src/router.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+import Alfred from "./views/Chatboard.vue";
 
 Vue.use(Router);
 
@@ -24,12 +25,8 @@ export default new Router({
     },
     {
       path: "/alfred",
-      name: "alfredt",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/Alfred.vue")
+      name: "Alfred",
+      component: Alfred
     }
   ]
 });
