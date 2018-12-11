e4226cfb40bd13e5c82b22844d5c38d02fbf7c0a
Vincent Kempers <me@vncn.tk>
2018-12-11 15:11:30 +0100

    add Chatbot test grounds as a view

---
+++ application/src/views/Chatboard.vue
@@ -0,0 +1,19 @@
+<template>
+  <section id="chatboard">
+    <Alfred/>
+    <Nav/>
+  </section>
+</template>
+
+<script>
+import Nav from "@/components/Navigation.vue";
+import Alfred from "@/components/Alfred.vue";
+
+export default {
+  name: "chatboard",
+  components: {
+    Alfred,
+    Nav
+  }
+};
+</script>
