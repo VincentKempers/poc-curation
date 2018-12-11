f7f8285efcb8773c0627b180d6df032d197d008b
Vincent Kempers <me@vncn.tk>
2018-12-11 17:03:10 +0100

    add content and add working carousel pages

--- application/src/components/HelloWorld.vue
+++ application/src/components/HelloWorld.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="hello">
-    <h1>Welkom bij de Nieuwe Boekhandel!</h1>
-    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+    <h3>Welkom bij de Nieuwe Boekhandel!</h3>
+    <p>
+      "Lorem ipsum dolodadr sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+      magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+      laboris nisi ut aliquip ex ea commodo consequat. "
+    </p>
   </div>
 </template>
 
@@ -16,18 +20,28 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.buttons {
+  position: absolute;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  right: 0;
+  left: 0;
+  bottom: 1em;
+
+  button {
+    border: none;
+    border-radius: 0.4em;
+    width: 8.5rem;
+    font-size: 1.1em;
+    padding: 0.55rem;
+  }
+  button:nth-child(1) {
+    background-color: #e5e6e8;
+  }
+  button:nth-child(2) {
+    background-color: #4da1ff;
+    color: white;
+  }
 }
 </style>
--- application/src/views/Home.vue
+++ application/src/views/Home.vue
@@ -1,18 +1,96 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <carousel
+      ref="carousel"
+      :perPage="1"
+      :navigationEnabled="false"
+      :paginationEnabled="false"
+      :scrollPerPage="true"
+      :mouseDrag="true"
+    >
+      <slide>
+        <img alt="Vue logo" src="../assets/logo.png">
+        <HelloWorld/>
+        <div class="buttons">
+          <button @click.prevent="prevSlide">Misschien later</button>
+          <button @click.prevent="nextSlide">Volgende</button>
+        </div>
+      </slide>
+      <slide>
+        <img alt="Vue logo" src="../assets/logo.png">
+        <HelloWorld/>
+        <div class="buttons">
+          <button @click.prevent="prevSlide">Misschien later</button>
+          <button @click.prevent="nextSlide">Ja natuurlijk!</button>
+        </div>
+      </slide>
+      <slide>
+        <img alt="Vue logo" src="../assets/logo.png">
+        <HelloWorld/>
+        <div class="buttons">
+          <button @click.prevent="prevSlide">Misschien later</button>
+          <button @click.prevent="nextSlide">Volgende</button>
+        </div>
+      </slide>
+    </carousel>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.VueCarousel-slide {
+  min-height: 98vh;
+}
+
+.buttons {
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  justify-items: center;
+  right: 0;
+  left: 0;
+  bottom: 0 !important;
+  z-index: +1;
+
+  button {
+    border: none;
+    border-radius: 0.4em;
+    width: 9rem;
+    font-size: 1.1em;
+    padding-top: 1.4rem;
+    padding-bottom: 1.4rem;
+  }
+  button:nth-child(1) {
+    background-color: #e5e6e8;
+  }
+  button:nth-child(2) {
+    background-color: #4da1ff;
+    color: white;
+  }
+}
+</style>
+
+
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import { Carousel, Slide } from "vue-carousel";
 
 export default {
   name: "home",
+  methods: {
+    nextSlide() {
+      this.$refs.carousel.goToPage(this.$refs.carousel.getNextPage());
+    },
+    prevSlide() {
+      this.$refs.carousel.goToPage(this.$refs.carousel.getPreviousPage());
+    }
+  },
   components: {
-    HelloWorld
+    HelloWorld,
+    Carousel,
+    Slide
   }
 };
 </script>
