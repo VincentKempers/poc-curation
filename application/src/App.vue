8033e57f917820cf4526b30218bb06e5002c61fa
Vincent Kempers <me@vncn.tk>
2018-12-11 12:48:29 +0100

    add navigation to the bottom

--- application/src/App.vue
+++ application/src/App.vue
@@ -1,11 +1,12 @@
 <template>
   <div id="app">
+    <router-view/>
+    <div id="navPlaceholder">navigation</div>
     <div id="nav">
       <router-link to="/">Home</router-link>|
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link>|
       <router-link to="/alfred">Alfred</router-link>
     </div>
-    <router-view/>
   </div>
 </template>
 
@@ -16,9 +17,35 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  max-width: 100vw;
+  max-height: 120vh;
 }
+
+#navPlaceholder {
+  background-color: grey;
+  padding: 15px;
+  position: relative;
+  bottom: 0;
+  z-index: +1;
+  width: 0;
+  left: 0;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+
 #nav {
-  padding: 30px;
+  background-color: grey;
+  padding: 15px;
+  position: fixed;
+  bottom: 0;
+  z-index: +1;
+  width: 92vw;
+  left: 0;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+
   a {
     font-weight: bold;
     color: #2c3e50;
