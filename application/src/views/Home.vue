6ca2132ae7bec1493e16e3e97cdf84d3b8812610
Vincent Kempers <me@vncn.tk>
2019-01-08 15:03:43 +0100

    add genres + small bugfix

--- application/src/views/Home.vue
+++ application/src/views/Home.vue
@@ -11,18 +11,17 @@
       <slide>
         <img alt="Vue logo" src="../assets/onboarding.svg">
         <HelloWorld/>
-        <div class="buttons">
-          <button @click.prevent="prevSlide">Misschien later</button>
-          <button @click.prevent="nextSlide">Volgende</button>
+        <div class="welcome-button button_local">
+          <button @click.prevent="nextSlide">Kies uw boekwinkel</button>
         </div>
       </slide>
       <slide>
         <img alt="Vue logo" src="../assets/onboarding-alfred.svg">
-        <h3>Mijn naam is Alfred</h3>
+        <h3>Kies uw boekwinkel</h3>
         <p>Ik ben hier om je te helpen met al jouw vragen, je te sturen binnen de app en om zo nu en dan een boek aan te raden! Zou je mij kunnen vertellen wat voor boeken je graag leest?</p>
-        <div class="buttons">
-          <button @click.prevent="prevSlide">Misschien later</button>
-          <button @click.prevent="nextSlide">Ja natuurlijk!</button>
+        <div class="buttons button_local">
+          <button class="white_button" @click.prevent="prevSlide">Misschien later</button>
+          <button class="blue_button" @click.prevent="nextSlide">Ja natuurlijk!</button>
         </div>
       </slide>
       <slide>
@@ -33,25 +32,40 @@
         </p>
 
         <div class="choices">
-          <label>
-            <input type="checkbox">genre
+          <label id="label">
+            <input type="checkbox">Detective
           </label>
           <label>
-            <input type="checkbox">genre
+            <input type="checkbox">Kinderboeken
           </label>
           <label>
-            <input type="checkbox">genre
+            <input type="checkbox">Science Fiction
           </label>
           <label>
-            <input type="checkbox">genre
+            <input type="checkbox"> Biografie
           </label>
           <label>
-            <input type="checkbox">genre
+            <input type="checkbox">Fantasy
+          </label>
+          <label>
+            <input type="checkbox">Roman
+          </label>
+          <label>
+            <input type="checkbox">Thriller
+          </label>
+          <label>
+            <input type="checkbox">Oorlog
+          </label>
+          <label>
+            <input type="checkbox">Literatuur
+          </label>
+          <label>
+            <input type="checkbox">Biografie
           </label>
         </div>
         <div class="buttons">
-          <button @click.prevent="prevSlide">Misschien later</button>
-          <button @click.prevent="nextSlide">Volgende</button>
+          <button class="white_button" @click.prevent="prevSlide">Misschien later</button>
+          <button class="blue_button" @click.prevent="nextSlide">Volgende</button>
         </div>
       </slide>
     </carousel>
@@ -63,12 +77,32 @@
   min-height: 98vh;
 }
 
+.button_local {
+  position: relative;
+  bottom: 0;
+  width: 100%;
+}
+
 img {
   width: 80%;
 }
 
+.welcome-button {
+  button {
+    background-color: #4da1ff;
+    color: white;
+    border: none;
+    border-radius: 0.4em;
+    width: 100%;
+    font-size: 1em;
+    font-weight: 500;
+    padding-top: 1.4rem;
+    padding-bottom: 1.4rem;
+    padding: 1.4em 1em 1.4em 1em;
+  }
+}
+
 .buttons {
-  position: relative;
   display: flex;
   flex-direction: row;
   justify-content: space-around;
@@ -76,21 +110,20 @@ img {
   right: 0;
   left: 0;
   bottom: 0 !important;
-  z-index: +1;
 
   button {
     border: none;
     border-radius: 0.4em;
-    width: 9rem;
     font-size: 1em;
     font-weight: 500;
     padding-top: 1.4rem;
     padding-bottom: 1.4rem;
+    padding: 1.4em 1em 1.4em 1em;
   }
-  button:nth-child(1) {
+  .white_button {
     background-color: #e5e6e8;
   }
-  button:nth-child(2) {
+  .blue_button {
     background-color: #4da1ff;
     color: white;
   }
@@ -100,19 +133,24 @@ img {
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
+  justify-content: center;
+  margin: 1em 0 1em 0;
   label {
     background-color: #e5e6e8;
     border: none;
-    border-radius: 0.4em;
-    margin: 1em;
-    width: 4rem;
+    border-radius: 0.6em;
+    margin: 0.5em;
     font-size: 1em;
     font-weight: 500;
-    padding: 1.4rem 0.8rem 1.4rem 0.8rem;
+    padding: 1em 0.8em 1em 0.8em;
 
     input {
       visibility: hidden;
       position: absolute;
+      color: red;
+      input:checked ~ #label {
+        color: red;
+      }
     }
   }
 }
