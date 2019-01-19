3c931a62215c0da1c5519c004861f417128b165f
Vincent Kempers <me@vncn.tk>
2019-01-19 17:11:37 +0100

    delete hello world

--- application/src/components/HelloWorld.vue
+++ application/src/components/HelloWorld.vue
@@ -1,8 +1,5 @@
 <template>
-  <div class="hello">
-    <h3>Welkom bij de Nieuwe Boekhandel!</h3>
-    <p>Welkom in de [naam] app! Om je opweg te helpen hebben wij een aantal boekwinkels bij jou in de buurt geselecteerd. Door een van deze boekwinkels te kiezen kunnen wij jou een gepersonaliseerd aanbod aanbieden.</p>
-  </div>
+  <div class="hello"></div>
 </template>
 
 <script>
@@ -13,8 +10,3 @@ export default {
   }
 };
 </script>
-
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-</style>
