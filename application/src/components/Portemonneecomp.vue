d78ebee6f089e250a3d9a62a571cd686e00dcd9b
Vincent Kempers <me@vncn.tk>
2019-01-27 15:22:27 +0100

    add styling to portemonnnee

--- application/src/components/Portemonneecomp.vue
+++ application/src/components/Portemonneecomp.vue
@@ -4,8 +4,8 @@
       <section class="bookscan">
         <img src="../assets/soloalfred.svg" alt="alfred">
         <article>
-          <h3>Welkom terug!</h3>
-          <span>Als je vragen of advies nodig hebt kun je bij mij wezen. Klik hier om met mij te praten.</span>
+          <h3>Welkom bij jouw portemonnee!</h3>
+          <span>Deel jouw code met je vrienden en ontvang een tegoed! Maximaal 3 vrienden in 1 week.</span>
         </article>
       </section>
       <div class="barcode">
@@ -33,15 +33,32 @@
           </div>
         </div>
       </div>
-
       <div class="events">
         <img src="../assets/calendar.png" alt="events icon">
         <h3>Deel je code</h3>
-        <span>Deel je code per week met 2 andere vrienden als zij een account aanmaken met jouw code, krijg jij 0,50 op je tegoed!</span>
+        <label id="copyComplete"></label>
         <div class="sidescroll">
-          <input type="text" value="3H13SD">
-          <button>Kopieer</button>
+          <input id="couponCode" :value="testingCode">
+          {{couponCode}}
+          <button @click.stop.prevent="copyTestingCode">Kopieer</button>
         </div>
+        <ul>
+          <li>
+            <a class="facebook" href>
+              <img src="../assets/facebook-white.svg" alt="facebook">
+            </a>
+          </li>
+          <li>
+            <a class="whatsapp" href>
+              <img src="../assets/whatsapp-logo.svg" alt="whatsapp">
+            </a>
+          </li>
+          <li>
+            <a class="twitter" href>
+              <img src="../assets/twitter-logo-silhouette-white.svg" alt="twitter">
+            </a>
+          </li>
+        </ul>
       </div>
     </div>
   </div>
@@ -151,11 +168,14 @@ h3 {
     article {
       text-align: left;
       font-size: 0.6em;
+      padding-left: 1.3em;
       h3 {
         color: #000;
+        font-size: 1.5em;
       }
       span {
         color: #000;
+        font-size: 1.2em;
       }
     }
   }
@@ -176,54 +196,68 @@ h3 {
     background-color: #4da1ff;
     border-radius: 5px;
     padding-left: 1em;
-    // padding-right: 1em;
     text-align: left;
     img {
       width: 0.8em;
       margin-top: 0.8em;
     }
     .sidescroll {
-      max-width: 100%;
-      border-radius: 5px;
-      ul {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin-top: 1.7em;
+      input {
+        border-radius: 0.4em 0em 0em 0.4em;
+        border: none;
+        padding: 0.4em;
+        font-size: 1.4em;
+        width: 25%;
+      }
+      button {
+        background-color: #e5e6e8;
+        color: black;
+        border: none;
+        border-radius: 0 0.4em 0.4em 0;
+        width: 6em;
+        font-size: 1em;
+        font-weight: 500;
+        padding: 0.84em;
+      }
+    }
+    ul {
+      display: flex;
+      flex-direction: row;
+      justify-content: center;
+      align-items: center;
+      padding: 0;
+      margin-top: 0.5em;
+      list-style: none;
+      li {
         display: flex;
-        flex-direction: row;
-        list-style: none;
-        margin: 0;
-        margin-top: 0.4em;
-        padding: 0;
-        padding-right: 1em;
-        overflow-x: scroll;
-        li {
-          margin-right: 0.5em;
-          .ticket {
-            padding: 0.4em;
-            border-radius: 5px;
-            background-color: white;
-            width: 10em;
-            margin-right: 0.7em;
-          }
-          #event {
+        justify-content: center;
+        text-align: center;
+        align-items: center;
+
+        a {
+          background-color: white;
+          padding: 1em;
+          width: 2em;
+          height: 1em;
+          img {
+            width: 1.3em;
+            text-align: center;
             margin: 0;
-            margin-bottom: 0.5em;
-            span {
-              font-size: 0.6em;
-              text-transform: uppercase;
-            }
-            h4 {
-              margin: 0;
-            }
+            color: white;
           }
-          #when {
-            margin: 0;
-            span {
-              font-size: 0.6em;
-              text-transform: uppercase;
-            }
-            h4 {
-              margin: 0;
-            }
-          }
+        }
+        .twitter {
+          background-color: #1da1f2;
+        }
+        .facebook {
+          background-color: #3c5999;
+        }
+        .whatsapp {
+          background-color: #1bd741;
         }
       }
     }
@@ -240,10 +274,34 @@ h3 {
     color: white;
   }
 }
+
+#extra-text {
+  display: none;
+}
 </style>
 
 <script>
 export default {
-  name: "Portemonneecomp"
+  name: "Portemonneecomp",
+  data() {
+    return { testingCode: "12HIK7" };
+  },
+  methods: {
+    copyTestingCode() {
+      let couponCode = document.querySelector("#couponCode");
+      couponCode.setAttribute("type", "text");
+      couponCode.select();
+
+      try {
+        var successful = document.execCommand("copy");
+        var copyComplete = document.querySelector("#copyComplete");
+        var msg = successful ? "successful" : "unsuccessful";
+        copyComplete.innerHTML = "code is succesvol gekopieerd";
+      } catch (err) {
+        copyComplete.innerHTML = "Oh er is iets misgegaan bij het kopieren";
+      }
+      window.getSelection().removeAllRanges();
+    }
+  }
 };
 </script>
